2dcd8487ccc0ae958009961c32c3ec24504b3c30
lisi <lisi@qq.com>
2019-05-20 15:23:54 +0800

    黄毛

--- src/com/test/TestDemo1.java
+++ src/com/test/TestDemo1.java
@@ -3,8 +3,9 @@ package com.test;
 public class TestDemo1 {
 
 	public static void main(String[] args) {
-		System.out.println("ÄãºÃ");
-		System.out.println("ÄãºÃ2");
+		System.out.println("é»„æ¯›");
+		System.out.println("é»„æ¯›2");
+		System.out.println("é»„æ¯›3");
 		System.err.println("hello0000000");
 
 	}
